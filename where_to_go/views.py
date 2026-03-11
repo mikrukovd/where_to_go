a47cd9b8002d7ce1689e68af035a99ffb8f5b64b
mikrukovd <z123456689z@gmail.com>
2026-03-11 13:50:32 +0300

    Move data from template to view and add json api

--- where_to_go/views.py
+++ where_to_go/views.py
@@ -1,5 +1,48 @@
 from django.shortcuts import render
+from places.models import Place
+from django.shortcuts import get_object_or_404
+from django.http import JsonResponse
 
 
 def show_index(request):
-    return render(request, 'index.html')
+    places = Place.objects.all()
+
+    places_geojson = {
+        "type": "FeatureCollection",
+        "features": []
+    }
+
+    for place in places:
+        feature = {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [place.lng, place.lat]
+            },
+            "properties": {
+                "title": place.title,
+                "placeId": place.id,
+                "detailsUrl": f'/places/{place.id}/'
+            }
+        }
+        places_geojson["features"].append(feature)
+
+    return render(request, 'index.html', context={'places_geojson': places_geojson})
+
+
+def place_detail(request, place_id):
+    place = get_object_or_404(Place, id=place_id)
+
+    response = {
+        "title": place.title,
+        "imgs": [img.image.url for img in place.images.all()],
+        "description_short": place.description_short,
+        "description_long": place.description_long,
+        "coordinates": {
+            "lng": place.lng,
+            "lat": place.lat
+        }
+    }
+
+    # json_dumps_params={'ensure_ascii': False} для русских букв
+    return JsonResponse(response, json_dumps_params={'ensure_ascii': False})
