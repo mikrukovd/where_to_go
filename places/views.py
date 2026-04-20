5b430b986d37c50afda7b402e4d4b03730e5f5c8
mikrukovd <z123456689z@gmail.com>
2026-04-20 13:12:01 +0300

    Update views.py

--- places/views.py
+++ places/views.py
@@ -6,25 +6,26 @@ from places.models import Place
 def show_index(request):
     places = Place.objects.all()
 
-    places_geojson = {
-        "type": "FeatureCollection",
-        "features": []
-    }
-
-    for place in places:
-        feature = {
-            "type": "Feature",
-            "geometry": {
-                "type": "Point",
-                "coordinates": [place.lng, place.lat]
+    features = [
+        {
+            'type': 'Feature',
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [place.lng, place.lat]
             },
-            "properties": {
-                "title": place.title,
-                "placeId": place.id,
-                "detailsUrl": f'/places/{place.id}/'
+            'properties': {
+                'title': place.title,
+                'placeId': place.id,
+                'detailsUrl': f'/places/{place.id}/'
             }
         }
-        places_geojson["features"].append(feature)
+        for place in places
+    ]
+
+    places_geojson = {
+        'type': 'FeatureCollection',
+        'features': features
+    }
 
     return render(request, 'index.html', context={'places_geojson': places_geojson})
 
@@ -33,13 +34,13 @@ def place_detail(request, place_id):
     place = get_object_or_404(Place, id=place_id)
 
     response = {
-        "title": place.title,
-        "imgs": [img.image.url for img in place.images.all()],
-        "description_short": place.description_short,
-        "description_long": place.description_long,
-        "coordinates": {
-            "lng": place.lng,
-            "lat": place.lat
+        'title': place.title,
+        'imgs': [img.image.url for img in place.images.all()],
+        'description_short': place.description_short,
+        'description_long': place.description_long,
+        'coordinates': {
+            'lng': place.lng,
+            'lat': place.lat
         }
     }
 
