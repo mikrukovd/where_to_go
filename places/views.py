357a84b53d42da774803cabfc502bf32c02446b5
mikrukovd <z123456689z@gmail.com>
2026-04-20 10:43:13 +0300

    Relocate views.py

--- places/views.py
+++ places/views.py
@@ -1,3 +1,47 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.shortcuts import get_object_or_404, render
+from places.models import Place
 
-# Create your views here.
+
+def show_index(request):
+    places = Place.objects.all()
+
+    places_geojson = {
+        "type": "FeatureCollection",
+        "features": []
+    }
+
+    for place in places:
+        feature = {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [place.lng, place.lat]
+            },
+            "properties": {
+                "title": place.title,
+                "placeId": place.id,
+                "detailsUrl": f'/places/{place.id}/'
+            }
+        }
+        places_geojson["features"].append(feature)
+
+    return render(request, 'index.html', context={'places_geojson': places_geojson})
+
+
+def place_detail(request, place_id):
+    place = get_object_or_404(Place, id=place_id)
+
+    response = {
+        "title": place.title,
+        "imgs": [img.image.url for img in place.images.all()],
+        "description_short": place.description_short,
+        "description_long": place.description_long,
+        "coordinates": {
+            "lng": place.lng,
+            "lat": place.lat
+        }
+    }
+
+    # json_dumps_params={'ensure_ascii': False} для русских букв
+    return JsonResponse(response, json_dumps_params={'ensure_ascii': False})
